66b8da0700f406785fce48f14dc85450fa453488
Corey Lehman <coreylehman@me.com>
2020-09-27 14:23:26 -0400

    Added the vjoy wrapper.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use tokio::time::{self, Duration};
 use keyboard_input::*;
+use vjoy_wrapper::{VJoyDevice, VJoyAxis};
 
 #[tokio::main]
 async fn main() {
     keyboard_input::initialize();
     keyboard_input::block_all_keys();
 
-    let mut interval = time::interval(Duration::from_millis(100));
+    let mut vjoy_device = VJoyDevice::new(1, "C:\\Program Files\\vJoy\\x64\\vJoyInterface.dll");
+
+    let mut interval = time::interval(Duration::from_millis(1));
     loop {
-        if keyboard_input::key_is_pressed(KeyboardKey::A) {
-            println!("Yee");
-        }
+        vjoy_device.set_button(1, keyboard_input::key_is_pressed(KeyboardKey::A));
+        vjoy_device.send_inputs();
 
         interval.tick().await;
     }
---
+++ src/vjoy_wrapper/src/lib.rs
@@ -0,0 +1,150 @@
+extern crate libloading as lib;
+
+use std::os::raw::{
+    c_uchar,
+    c_long,
+    c_uint,
+};
+
+#[allow(non_snake_case)]
+#[repr(C)]
+#[derive(Default, Copy, Clone)]
+struct JoystickPositionV2 {
+    /// JOYSTICK_POSITION
+    bDevice: c_uchar, // Index of device. 1-based.
+    wThrottle: c_long,
+    wRudder: c_long,
+    wAileron: c_long,
+    wAxisX: c_long,
+    wAxisY: c_long,
+    wAxisZ: c_long,
+    wAxisXRot: c_long,
+    wAxisYRot: c_long,
+    wAxisZRot: c_long,
+    wSlider: c_long,
+    wDial: c_long,
+    wWheel: c_long,
+    wAxisVX: c_long,
+    wAxisVY: c_long,
+    wAxisVZ: c_long,
+    wAxisVBRX: c_long,
+    wAxisVBRY: c_long,
+    wAxisVBRZ: c_long,
+    lButtons: c_long, // 32 buttons: 0x00000001 means button1 is pressed, 0x80000000 -> button32 is pressed
+    bHats: c_uint, // Lower 4 bits: HAT switch or 16-bit of continuous HAT switch
+    bHatsEx1: c_uint, // Lower 4 bits: HAT switch or 16-bit of continuous HAT switch
+    bHatsEx2: c_uint, // Lower 4 bits: HAT switch or 16-bit of continuous HAT switch
+    bHatsEx3: c_uint, // Lower 4 bits: HAT switch or 16-bit of continuous HAT switch
+
+    /// JOYSTICK_POSITION_V2 Extension
+    lButtonsEx1: c_long, // Buttons 33-64
+    lButtonsEx2: c_long, // Buttons 65-96
+    lButtonsEx3: c_long, // Buttons 97-128
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum VJoyAxis {
+    X,
+    Y,
+    Z,
+    XRotation,
+    YRotation,
+    Slider0,
+}
+
+pub struct VJoyDevice {
+    device_id: u32,
+    vjoy: lib::Library,
+    state: JoystickPositionV2,
+}
+
+impl VJoyDevice {
+    pub fn new(device_id: u32, path: &str) -> Self {
+        let output = Self {
+            device_id,
+            vjoy: lib::Library::new(path).expect("Could not load vJoyInterface.dll."),
+            state: JoystickPositionV2::default(),
+        };
+        if output.v_joy_is_enabled() {
+            output.acquire_vjoy_device();
+        }
+        output
+    }
+
+    pub fn set_button(&mut self, button_id: u32, state: bool) {
+        let bit_index: u32 = button_id - 1;
+        if state {
+            self.state.lButtons = self.state.lButtons | (1 << bit_index);
+        }
+        else {
+            self.state.lButtons = self.state.lButtons & !(1 << bit_index);
+        }
+    }
+
+    pub fn set_axis(&mut self, axis: VJoyAxis, value: f32) {
+        match axis {
+            VJoyAxis::X => self.state.wAxisX = Self::get_scaled_axis_value(value),
+            VJoyAxis::Y => self.state.wAxisY = Self::get_scaled_axis_value(value),
+            VJoyAxis::Z => self.state.wAxisZ = Self::get_scaled_axis_value(value),
+            VJoyAxis::XRotation => self.state.wAxisXRot = Self::get_scaled_axis_value(value),
+            VJoyAxis::YRotation => self.state.wAxisYRot = Self::get_scaled_axis_value(value),
+            VJoyAxis::Slider0 => self.state.wSlider = Self::get_scaled_slider_value(value),
+        }
+    }
+
+    pub fn send_inputs(&self) {
+        unsafe {
+            let func = self.vjoy.get::<lib::Symbol<unsafe extern fn(u32, JoystickPositionV2) -> u32>>(b"UpdateVJD")
+                .expect(&format!("Could not update vJoy device {}.", self.device_id));
+            if func(self.device_id, self.state) == 0 {
+                panic!("Could not update vJoy device {}.", self.device_id);
+            };
+        }
+    }
+
+    fn get_scaled_axis_value(value: f32) -> c_long {
+        let scaled_value = 0.5 * (0.626 * value + 1.0);
+        let asdf = (scaled_value * (0x8000 as f32)) as c_long;
+        println!("{}", asdf);
+        return (scaled_value * (0x8000 as f32)) as c_long;
+    }
+
+    fn get_scaled_slider_value(value: f32) -> c_long {
+        return (value * (0x8000 as f32)) as c_long;
+    }
+
+    fn v_joy_is_enabled(&self) -> bool {
+        unsafe {
+            if let Ok(func) = self.vjoy.get::<lib::Symbol<unsafe extern fn() -> u32>>(b"vJoyEnabled") {
+                if func() == 1 { true } else { false }
+            }
+            else {
+                false
+            }
+        }
+    }
+
+    fn acquire_vjoy_device(&self) {
+        unsafe {
+            let func = self.vjoy.get::<lib::Symbol<unsafe extern fn(u32) -> u32>>(b"AcquireVJD")
+                .expect(&format!("Could not acquire vJoy device {}.", self.device_id));
+            if func(self.device_id) == 0 {
+                panic!("Could not acquire vJoy device {}.", self.device_id);
+            };
+        }
+    }
+
+    fn free_vjoy_device(&self) {
+        unsafe {
+            let func = self.vjoy.get::<lib::Symbol<unsafe extern fn(u32)>>(b"RelinquishVJD")
+                .expect(&format!("Could not free vJoy device {}.", self.device_id));
+            func(self.device_id);
+        }
+    }
+}
+
+impl Drop for VJoyDevice {
+    fn drop(&mut self) {
+        self.free_vjoy_device();
+    }
+}
