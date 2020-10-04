d0d696486d61440960d859668c53bbb8adf57115
Corey Lehman <coreylehman@me.com>
2020-10-04 16:44:49 -0400

    Switch to a different method for calling the main function periodically.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::time::Duration;
+use std::thread;
+use std::time::{Instant, Duration};
 use keyboard_melee_controller::KeyboardMeleeController;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let mut controller = KeyboardMeleeController::new();
 
-    let mut interval = tokio::time::interval(Duration::from_millis(1));
+    let update_rate = Duration::from_millis(1);
+
+    //let mut time_of_last_loop = Instant::now();
+    let mut time_of_next_loop = Instant::now();
     loop {
+        //println!("{:?}", Instant::now() - time_of_last_loop);
+        //time_of_last_loop = Instant::now();
+
+        time_of_next_loop += update_rate;
+
         controller.update();
-        interval.tick().await;
+
+        let now = Instant::now();
+        if time_of_next_loop > now {
+            thread::sleep(time_of_next_loop - now);
+        }
     }
 }
